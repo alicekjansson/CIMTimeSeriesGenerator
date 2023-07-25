06a8eebcb98135a6c7251df74fdb8291ef683396
alicekjansson <alice.jansson@iea.lth.se>
2023-07-25 11:03:00 +0200

    Update typkurvor.py

--- typkurvor.py
+++ typkurvor.py
@@ -15,37 +15,30 @@ import PySimpleGUI as sg
 sg.theme('DarkTeal4')
 layout = [[sg.Text('Define the requirements of time series')],
           [sg.Text('Choose dwelling:')],
-          [sg.Combo(['Småhus Direktel','Småhus Hushållsel','Lägenhet','Industri'])],
+          [sg.Combo(['Småhus Direktel','Småhus Hushållsel','Lägenhet','Industri'],key='TYP',enable_events=True)],
           [sg.Text('Choose bidding area:')],
-          [sg.Combo(['SE1','SE2','SE3','SE4'])],
+          [sg.Combo(['SE1','SE2','SE3','SE4'],key='ZONE',enable_events=True)],
           [sg.Text('Choose season:')],
-          [sg.Combo(['Winter','Autumn/Spring','Summer'])],
+          [sg.Combo(['Winter','Autumn/Spring','Summer'],key='SEASON',enable_events=True)],
           [sg.Text('Choose day:')],
-          [sg.Combo(['Weekday','Weekend'])],
+          [sg.Combo(['Weekday','Weekend'],key='DAY',enable_events=True)],
           [sg.Submit('Generate Timeseries'),sg.Exit()]]
 
 # Create the window
 window = sg.Window('Timeseries Generator', layout)
 
-# Make choices
-
+# TYP
 # 0 Småhus direktel
 # 1 Småhus hushållsel
 # 2 Lägenhet
 # 3 Industri
-typ=0
-
-# Välj elområde
-elomr=2
-
+# SEASON
 # 0 Vinter
 # 1 Vår/Höst
 # 2 Sommar
-arstid=2
-
+# DAY
 # 0 Vardag
 # 1 Helg och helgdag
-dag=0
 
 dwellings=['Småhus Direktel','Småhus Hushållsel','Lägenhet','Industri']
 bids=['SE1','SE2','SE3','SE4']
@@ -58,18 +51,27 @@ while True:
     # End program if user closes window
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    for i,d in enumerate(dwellings):
-        if event == d:
-            typ=i
-    for i,d in enumerate(bids):
-        if event == d:
-            elomr=i+1
-    for i,d in enumerate(seasons):
-        if event == d:
-            arstid=i
-    for i,d in enumerate(days):
-        if event == d:
-            dag=i
+    
+    if values['TYP']:
+        choice=values['TYP']                # Get user choice
+        for i,d in enumerate(dwellings):    # Get user choice in int form
+            if choice == d:
+                typ=i                       # Update house dwelling type
+    if values['ZONE']:
+        choice=values['ZONE']                # Get user choice
+        for i,d in enumerate(bids):
+            if choice == d:
+                elomr=i+1
+    if values['SEASON']:
+        choice=values['SEASON']                # Get user choice
+        for i,d in enumerate(seasons):
+            if choice == d:
+                arstid=i
+    if values['DAY']:
+        choice=values['DAY']                # Get user choice
+        for i,d in enumerate(days):
+            if choice == d:
+                dag=i
     if event == 'Generate Timeseries':
         P=generate_timeseries(typ,elomr,arstid,dag)
         break
@@ -78,10 +80,10 @@ window.close()
 
 
 # TO-DO:
-# Make selections in GUI change values in script
 # Add to GUI that P is saved as csv 
 # Add option to select yearly or daily profile
 # Deal with industry load
 # Aggregate load profiles to higher voltage levels
 # Add selection of voltage level / average power
 # Deal with generation
+# Add option to randomize load curves slightly?
